593f2a1467ecb7da0a9a1b6346a0e03ec20292f6
Alish <alish.gilenov@mail.ru>
2020-11-22 18:51:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import React, { Layout } from './hoc/Layout/Layout.jsx'
+
+function App() {
+   return (
+      <Layout>
+         <div style={{ width: 400, border: '1px solid black' }}>
+            <h1>Layout works</h1>
+         </div>
+      </Layout>
+   );
+}
+
+export default App;
